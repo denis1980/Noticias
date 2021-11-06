6ae93c1b4949e7a5deee6b5c7e9728d90b39321a
Denis <denisvalerio.stm@gmail.com>
2021-11-06 14:31:30 -0400

    Simulação de nova funcionalidade

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::resource('noticias', NoticiaController::class);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/teste', function() {});
